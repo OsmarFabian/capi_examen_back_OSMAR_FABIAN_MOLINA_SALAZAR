8cf40cd17f0bbcc8455f6d3fbacd326c6bd16f96
OsmarFabian <osmarsalaazar@hotmail.com>
2021-10-15 19:08:15 -0500

    ultimos puntos del examen

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::with('direccion')->get();
+        foreach ($users as $user) {
+            $anio_nacimiento = '';
+            $edad = Carbon::parse($user->fecha_nacimiento)->age;
+            $user->edad=$edad;
+        }
+        return $users;
+    }
+}
---
+++ app/Models/Userdomicilio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Userdomicilio extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_domicilio';
+}
